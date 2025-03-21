e214ff77cf0600ef6105d83b1c9dcaa9fdcd01f0
Brian Beattie <beattie@beattie-home.net>
2025-03-20 18:53:33 -0700

    move main.rs to examples/hello, new main.rs with timer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
@@ -5,10 +6,22 @@ use panic_rtt_target as _;
 use rtt_target::{rtt_init_print, rprintln};
 
 use cortex_m_rt::entry;
+use microbit::board::Board;
+use microbit::hal::{prelude::*, Timer};
 
 #[entry]
 fn main() -> ! {
+    let mut tick = 0;
+
     rtt_init_print!();
     rprintln!("Hello World");
-    loop {}
+
+    let mut board = Board::take().unwrap();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    loop {
+        tick += 1;
+        timer.delay_ms(1_000u32);
+    }
 }
